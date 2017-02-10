7968b2c3e27e4ab3d7bde90dd19735eef893cde5
hexiuwei <abchxw@gmail.com>
2017-02-10 21:37:26 +0800

    change admin/product backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,7 +38,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-     params.require(:product).permit(:title, :description, :quqntity, :price)
+     params.require(:product).permit(:title, :description, :quqntity, :price, :image)
   end
 
 end
